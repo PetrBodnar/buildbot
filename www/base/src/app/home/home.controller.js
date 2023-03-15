a97bd038f7b5b9c17ad0c306e7bfbf8cb8cb3e1f
Petr Bodnar <p.bodnar@lightmap.com>
2023-03-15 15:22:29 +0400

    added 20 builds for each builder

--- www/base/src/app/home/home.controller.js
+++ www/base/src/app/home/home.controller.js
@@ -10,44 +10,54 @@ class Home {
         $scope.config = config;
 
         const data = dataService.open().closeOnDestroy($scope);
-        $scope.buildsRunning = data.getBuilds({order: '-started_at', complete: false, property: ["owners"]});
-        $scope.recentBuilds = data.getBuilds({order: '-buildid', complete: true, limit:20, property: ["owners"]});
         $scope.builders = data.getBuilders();
-        $scope.hasBuilds = b => (b.builds != null ? b.builds.length : undefined) > 0 || 
+        $scope.hasBuilds = b => (b.recentBuilds != null ? b.recentBuilds.length : undefined) > 0 || 
             (b.buildsRunning != null ? b.buildsRunning.length : undefined) > 0;
 
-        const updateBuilds = function() {
-            const byNumber = (a, b) => a.number - b.number;
-
-            $scope.buildsRunning.forEach(function(build) {
-                const builder = $scope.builders.get(build.builderid);
-                if (builder != null) {
-                    if (builder.buildsRunning == null) { builder.buildsRunning = []; }
-                    if (builder.buildsRunning.indexOf(build) < 0) {
-                        builder.buildsRunning.push(build);
-                        builder.buildsRunning.sort(byNumber);
-                    }
-                }
-            });
-            
-            let forEach = $scope.recentBuilds.forEach(function(build) {
-                const builder = $scope.builders.get(build.builderid);
-                if (builder != null) {
-                    if (builder.builds == null) { builder.builds = []; }
-                    if (builder.builds.indexOf(build) < 0) {
-                        builder.builds.push(build);
-                        builder.builds.sort(byNumber);
-                    }
-                }
-            });
-            return forEach;
-        };
-
-        $scope.buildsRunning.onChange = updateBuilds;
-        $scope.recentBuilds.onChange = updateBuilds;
-        $scope.builders.onChange = updateBuilds;
-
         $scope.builders.onNew = function(builder) {
+            // const byNumber = (a, b) => a.number - b.number;
+            
+            let onNewBuild = function(build) {
+                build.getProperties().onNew = function(properties)
+                {
+                    build.buildProperties = properties;
+                };
+                data.getBuildrequests(build.buildrequestid).onNew = function(buildrequest) {
+                    data.getBuildsets(buildrequest.buildsetid).onNew = function(buildset) {
+                        build.branch = buildset.sourcestamps[0].branch;
+                        build.owner = build.properties.owners[0][0];
+                    };
+                };
+            };
+
+            builder.buildsRunning = data.getBuilds({order: '-started_at', complete: false, property: ["owners"], builderid__eq:[builder.builderid]});
+            builder.recentBuilds = data.getBuilds({order: '-buildid', complete: true, limit:20, property: ["owners"], builderid__eq:[builder.builderid]});
+
+            builder.buildsRunning.onNew = onNewBuild;
+            builder.recentBuilds.onNew = onNewBuild;
+
+            // builder.buildsRunning.forEach(function(build) {
+            //     const builder = $scope.builders.get(build.builderid);
+            //     if (builder != null) {
+            //         if (builder.buildsRunning.indexOf(build) < 0) {
+            //             builder.buildsRunning.push(build);
+            //             builder.buildsRunning.sort(byNumber);
+            //         }
+            //     }
+            // });
+            //
+            // $scope.recentBuilds.forEach(function(build) {
+            //     const builder = $scope.builders.get(build.builderid);
+            //     if (builder != null) {
+            //         if (builder.builds == null) { builder.builds = []; }
+            //         if (builder.builds.indexOf(build) < 0) {
+            //             builder.builds.push(build);
+            //             builder.builds.sort(byNumber);
+            //         }
+            //     }
+            // });
+            
+            
             builder.getForceschedulers().onChange = function(forceschedulers)
             {
                 builder.forceBuild = function () {
@@ -57,22 +67,6 @@ class Home {
                 };
             };
         };
-
-        let onNewBuild = function(build) {
-            build.getProperties().onNew = function(properties)
-            {
-                build.buildProperties = properties;
-            };
-            data.getBuildrequests(build.buildrequestid).onNew = function(buildrequest) {
-                data.getBuildsets(buildrequest.buildsetid).onNew = function(buildset) {
-                    build.branch = buildset.sourcestamps[0].branch;
-                    build.owner = build.properties.owners[0][0];
-                };
-            };
-        };
-
-        $scope.buildsRunning.onNew = onNewBuild;
-        $scope.recentBuilds.onNew = onNewBuild;
     }
 }
 
