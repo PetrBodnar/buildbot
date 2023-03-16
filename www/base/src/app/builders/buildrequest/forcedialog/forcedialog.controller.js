a19a1fb31eb2c9262449d5ea2cd4494eeae83efb
Petr Bodnar <p.bodnar@lightmap.com>
2023-03-16 18:01:50 +0400

    added hiding properties unused for bundles

--- www/base/src/app/builders/buildrequest/forcedialog/forcedialog.controller.js
+++ www/base/src/app/builders/buildrequest/forcedialog/forcedialog.controller.js
@@ -7,7 +7,7 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 class forceDialog {
-    constructor($scope, config, $state, modal, schedulerid, $rootScope, builderid, dataService) {
+    constructor($scope, config, $state, modal, schedulerid, $rootScope, builderid, dataService, $interval) {
         dataService.getForceschedulers(schedulerid, {subscribe: false}).onChange = function(schedulers) {
             const scheduler = schedulers[0];
             const all_fields_by_name = {};
@@ -39,6 +39,41 @@ class forceDialog {
                 }
             };
 
+            const stop = $interval(() => {
+                let selects = document.getElementsByTagName('select');
+                if (selects.length == 0)
+                    return;
+
+                $interval.cancel(stop);
+
+                for (let i = 0; i < selects.length; i++) {
+                    selects[i].addEventListener('change', function() {
+                        if(!Array.apply(null, this.options).some(element => element.label === "Bundles"))
+                            return;
+
+                        let display = this.value.endsWith("Bundles") ? "none" : "block";
+                        let props = ["Obfuscate", "Profiler", "App Store", "Test Flight", "Use Obb", "Split Arch", "Android Store", "Version Code"];
+                        for (let j = 0; j < props.length; j++) {
+                            let prop = props[j];
+                            let xpath = `//label[text()[contains(.,'${prop}')]]`;
+                            let node = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                            if(!node)
+                                continue;
+                            
+                            if(prop === "Android Store" || prop === "Version Code")
+                            {
+                                node.parentElement.style.display = display;
+                            }
+                            else
+                            {
+                                node.style.display = display;
+                            }
+                        }
+
+                    });
+                }
+            });
+            
             prepareFields(scheduler.all_fields);
             angular.extend($scope, {
                 rootfield: {
@@ -90,4 +125,4 @@ class forceDialog {
 
 
 angular.module('app')
-.controller('forceDialogController', ['$scope', 'config', '$state', 'modal', 'schedulerid', '$rootScope', 'builderid', 'dataService', forceDialog]);
+.controller('forceDialogController', ['$scope', 'config', '$state', 'modal', 'schedulerid', '$rootScope', 'builderid', 'dataService', '$interval', forceDialog]);
